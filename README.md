f4f4b48918c1fe6ed91bd3cb364367e6cdefb678
selinayr <ryuan@uw.edu>
2023-03-07 21:35:33 -0800

    PS6

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # PS6-creatingawebapp
+- In the first panel, I used satellite temperature data from UAH, and temperature *temp* is measured as deviation (deg C) from 1991-2020 baseline. The dataset contains 14310 observations and 4 variables.You can choose a random sample of data for 10, 25, 50, or 100 entries. 
+
+- In the second panel, I made a plot analyzing the global temperature for different regions. You can select the regions you are interested in, and you will see a monthly scatterplot and the corresponding trend lines. The plot will generate a distribution for the region(s) selected. The plot shows the year-month as the x-axis, a random color to each filtered region, a trend line if the user wants them, and the number of observations in the subset and range of the dates. 
+
+In the third panel, I created a table which displays average temperature over different time periods, *month*, *year*, and *decades*. It filter the data based on the user's selection. The wedgit includes a radio button group to select the time period to display, and the output will include a text to show the temperature range and a table of the data. 
--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(rsconnect)
 
 data <- read_delim("UAH-lower-troposphere-long.csv.bz2")
 
@@ -29,25 +30,15 @@ ui <- fluidPage(
     tabPanel("plot",
              sidebarLayout(
                sidebarPanel(
-                 # text explaining the plot and options
                  p("You can analyze the global temperature for different regions. Select the regions you are interested in. You see a monthly scatterplot and the corresponding trend lines."),
-                 
-                 # a checkbox asking if the user wants to see the trend lines
                  checkboxInput("trend", "Show trend lines", TRUE),
-                 
-                 # a radio button group to select the color of the trend lines
                  radioButtons("color", "Palette",
                               choices = c("orangered", "green")),
-                 
-                 # a checkbox group to select the regions to display, default select `global`
                  checkboxGroupInput("region", "Select the region(s) to display:",
                                     choices = unique(data$region), selected = "globe")
                ),
                mainPanel(
-                 # Display a plot of the generated distribution
                  plotOutput("plot"),
-                 
-                 # a text to display the number of observations in the subset and range of the dates
                  textOutput("plotDesc")
                )
              )
@@ -64,10 +55,7 @@ ui <- fluidPage(
                               choices = c("month", "year", "decade"), selected = "month")
                ),
                mainPanel(
-                 # a text to show the temperature range
                  textOutput("tablePanelPeriodDesc"),
-                 
-                 # Display a table of the data
                  dataTableOutput("tablePanelPeriod")
                  
                )
@@ -79,30 +67,12 @@ ui <- fluidPage(
 
 # Define the server
 server <- function(input, output) {
-  ############################
-  # General Info
-  ############################
-  
-  # Generate a short random sample of the data as a table (random 10) to display on "General Info" page
   output$sampleMain <- renderDataTable({
     data[sample(nrow(data), 10), ]
   })
-  
-  
-  
-  ############################
-  # Plot
-  ############################
-  
-  # for the data, convert year and month to a date
   data$month <- as.Date(paste(data$year, data$month, "01", sep = "-"))
-  
-  # Generate a plot of the generated distribution
   output$plot <- renderPlot({
-    # filter the data based on the user's selection
     dataFiltered <- data %>% filter(region %in% input$region)
-    
-    # display `dataFiltered` in the console
     print(dataFiltered)
     
     # plot the data
@@ -111,14 +81,9 @@ server <- function(input, output) {
     p <- ggplot(dataFiltered, aes(x = month, y = temp, color = region)) +
       geom_point() +
       labs(x = "Year-Month", y = "Temperature (deg C)", color = "Region")
-    
-    
-    # add trend lines if the user wants them
     if (input$trend) {
       p <- p + geom_smooth(method = "lm", se = FALSE, color = input$color)
     }
-    
-    # display the number of observations in the subset and range of the dates
     output$plotDesc <- renderText({
       paste("Time range:", min(dataFiltered$month), "to", max(dataFiltered$month),
             "Number of observations:", nrow(dataFiltered))
@@ -127,30 +92,7 @@ server <- function(input, output) {
     p
     
   })
-  
-  ############################
-  # Table
-  ############################
-  
-  # Generate a table of the data
   output$tablePanelPeriod <- renderDataTable({
-    # filter the data based on the user's selection
-    # if user selects `month`, 
-    # then sort by `year` and `month`,
-    # and display the average temperature for each month.
-    # columns are `year`, `month`, `temp`
-    
-    # if user selects `year`,
-    # then sort by `year`,
-    # and display the average temperature for each year.
-    # columns are `year`, `temp`
-    
-    # if user selects `decade`,
-    # then sort by `decade`,
-    # and display the average temperature for each decade.
-    # columns are `decade`, `temp`
-    
-    # display `dataFiltered` in the console
     print(data)
     
     isMonth <- input$timePeriod == "month"
@@ -174,8 +116,6 @@ server <- function(input, output) {
         summarise(temp = mean(temp)) %>% 
         arrange(decade)
     }
-    
-    # a text to show the temperature range
     output$tablePanelPeriodDesc <- renderText({
       paste("Temperature range:", min(data$temp), "to", max(data$temp))
     })
@@ -189,13 +129,9 @@ server <- function(input, output) {
 }
 
 tryCatch(
-  
-  # Run the application
   shinyApp(ui = ui, server = server,
            options=list(port=3838, launch.browser=FALSE)
            , onStart=cat("starting shiny server...\n")),
-  
-  # this is for the case when the user presses Ctrl+C to stop the app from the console
   interrupt = function(e) {
     cat("Exiting app by key-interrupt...")
     stopApp()		
